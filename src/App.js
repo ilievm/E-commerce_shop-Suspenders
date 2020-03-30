2b42e195063e4caf2c0db3f14e422ad30e3edfaf
unknown <mikluazshnorkel@gmail.com>
2020-03-29 20:59:12 -0700

    second attempt of uploading changes to collection page and styles

--- src/App.js
+++ src/App.js
@@ -4,12 +4,7 @@ import {Route, Switch} from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage';
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats page</h1>
-  </div>
-);
+import ShopPage from './pages/shop/shop.component'
 
 
 function App() {
@@ -17,7 +12,7 @@ function App() {
     <div className="App">
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/hats' component={HatsPage} />
+        <Route exact path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
